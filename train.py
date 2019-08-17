6168d276e31a4d2e1aeede6beb799354672a1d55
d4tah0g <k4ff33p0tt@gmail.com>
2019-08-17 14:23:16 +0200

    removed debug print

--- train.py
+++ train.py
@@ -122,10 +122,7 @@ def load_or_create_model(config_path, dcgan_path, discriminator_path,
 def train_batch(X_train, batch_size, dcgan, discriminator, generator, index,
                 y_d_gen, y_d_true, y_g):
     X_d_true = X_train[index * batch_size:(index + 1) * batch_size]
-    print(type(X_d_true))
-    X_d_true_foo = X_d_true *17.5
-    print(X_d_true_foo[0,0,0,0])
-    print(X_train[index * batch_size:(index + 1) * batch_size][0,0,0,0])
+
     #X_d_true = X_d_true.view(dtype=np.float32, type=np.ndarray)
     X_g = np.array([np.random.normal(0, 0.5, 100) for _ in range(batch_size)])
     X_d_gen = generator.predict(X_g, verbose=0)
