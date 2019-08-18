72a3b19cb84f8695031bb4b96459a7fd419500e0
d4tah0g <k4ff33p0tt@gmail.com>
2019-08-18 17:26:27 +0200

    Disc change

--- model.py
+++ model.py
@@ -15,7 +15,7 @@ def generator(input_dim=100,units=1024,activation='relu'):
     generator = Sequential()
 
     # FC: 2x2x512
-    generator.add(Dense(2*2*512, kernel_initializer=init))
+    generator.add(Dense(2*2*512,input_shape=(input_dim,), kernel_initializer=init))
     generator.add(Reshape((2, 2, 512)))
     generator.add(BatchNormalization())
     generator.add(LeakyReLU(0.2))
