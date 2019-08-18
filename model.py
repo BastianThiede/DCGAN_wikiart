53196e08e9be43c55f070474fa9d2a9687efb87c
d4tah0g <k4ff33p0tt@gmail.com>
2019-08-18 13:39:47 +0200

    Added Input noise, upsized Discriminator

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 from keras.initializers import RandomNormal
 from keras.layers import (ZeroPadding2D,
                           Conv2DTranspose, Conv2D, BatchNormalization, Reshape,
-                          Dense, LeakyReLU,
+                          Dense, LeakyReLU,GaussianNoise,
                           Flatten, UpSampling2D)
 from keras.models import Sequential
 from utils import load_config
@@ -53,20 +53,25 @@ def discriminator(input_shape=(32, 32, 3),nb_filter=64):
     discriminator = Sequential()
 
     # Conv 1: 16x16x64
-    discriminator.add(Conv2D(16, kernel_size=5, strides=2, padding='same',
-                             input_shape=(64,64,3), kernel_initializer=init))
+    discriminator.add(GaussianNoise(input_shape=(64, 64, 3)))
+
+    discriminator.add(Conv2D(32, kernel_size=5, strides=2, padding='same',
+                            kernel_initializer=init))
     discriminator.add(LeakyReLU(0.2))
 
     # Conv 2:
-    discriminator.add(Conv2D(32, kernel_size=5, strides=2, padding='same'))
-    discriminator.add(BatchNormalization())
-    discriminator.add(LeakyReLU(0.2))
-
-    # Conv 3:
     discriminator.add(Conv2D(64, kernel_size=5, strides=2, padding='same'))
     discriminator.add(BatchNormalization())
     discriminator.add(LeakyReLU(0.2))
 
+    # Conv 3:
+    discriminator.add(Conv2D(128, kernel_size=5, strides=2, padding='same'))
+    discriminator.add(BatchNormalization())
+    discriminator.add(LeakyReLU(0.2))
+
+    discriminator.add(Conv2D(256, kernel_size=5, strides=2, padding='same'))
+    discriminator.add(BatchNormalization())
+    discriminator.add(LeakyReLU(0.2))
 
     # FC
     discriminator.add(Flatten())
