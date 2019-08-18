d428c20e6116ef3875a634e205154e1873e9b167
d4tah0g <k4ff33p0tt@gmail.com>
2019-08-18 18:36:27 +0200

    Model testing

--- model.py
+++ model.py
@@ -51,21 +51,21 @@ def discriminator(input_shape=(32, 32, 3),nb_filter=64):
 
     # Conv 1: 16x16x64
 
-    discriminator.add(Conv2D(16, input_shape=(64, 64, 3), kernel_size=5, strides=2, padding='same',
+    discriminator.add(Conv2D(32, input_shape=(64, 64, 3), kernel_size=5, strides=2, padding='same',
                             kernel_initializer=init))
     discriminator.add(LeakyReLU(0.2))
 
     # Conv 2:
-    discriminator.add(Conv2D(32, kernel_size=5, strides=2, padding='same'))
-    discriminator.add(BatchNormalization())
-    discriminator.add(LeakyReLU(0.2))
-
-    # Conv 3:
     discriminator.add(Conv2D(64, kernel_size=5, strides=2, padding='same'))
     discriminator.add(BatchNormalization())
     discriminator.add(LeakyReLU(0.2))
 
+    # Conv 3:
     discriminator.add(Conv2D(128, kernel_size=5, strides=2, padding='same'))
+    discriminator.add(BatchNormalization())
+    discriminator.add(LeakyReLU(0.2))
+
+    discriminator.add(Conv2D(256, kernel_size=5, strides=2, padding='same'))
     discriminator.add(BatchNormalization())
     discriminator.add(LeakyReLU(0.2))
 
