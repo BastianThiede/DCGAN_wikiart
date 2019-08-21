4fb2670c56489cba2c03b5969565cd373b7c73e4
d4tah0g <k4ff33p0tt@gmail.com>
2019-08-21 22:50:19 +0200

    Momentum

--- model.py
+++ model.py
@@ -57,16 +57,16 @@ def discriminator(input_shape=(32, 32, 3),nb_filter=64):
 
     # Conv 2:
     discriminator.add(Conv2D(64, kernel_size=5, strides=2, padding='same'))
-    discriminator.add(BatchNormalization(momentum=0.2))
+    discriminator.add(BatchNormalization())
     discriminator.add(LeakyReLU(0.2))
 
     # Conv 3:
     discriminator.add(Conv2D(128, kernel_size=5, strides=2, padding='same'))
-    discriminator.add(BatchNormalization(momentum=0.2))
+    discriminator.add(BatchNormalization())
     discriminator.add(LeakyReLU(0.2))
 
     discriminator.add(Conv2D(256, kernel_size=5, strides=2, padding='same'))
-    discriminator.add(BatchNormalization(momentum=0.2))
+    discriminator.add(BatchNormalization())
     discriminator.add(LeakyReLU(0.2))
 
     # FC
