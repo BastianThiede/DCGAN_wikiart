0ba915e04f66b672c38341ea779d1bf82691019c
d4tah0g <k4ff33p0tt@gmail.com>
2019-08-22 12:49:12 +0200

    Added layer to discriminator

--- model.py
+++ model.py
@@ -60,6 +60,10 @@ def discriminator(input_shape=(32, 32, 3),nb_filter=64):
     discriminator.add(BatchNormalization())
     discriminator.add(LeakyReLU(0.2))
 
+    discriminator.add(Conv2D(64, kernel_size=5, strides=2, padding='same'))
+    discriminator.add(BatchNormalization())
+    discriminator.add(LeakyReLU(0.2))
+
     # Conv 3:
     discriminator.add(Conv2D(128, kernel_size=5, strides=2, padding='same'))
     discriminator.add(BatchNormalization())
