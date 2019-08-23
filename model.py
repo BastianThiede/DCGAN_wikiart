ac84e5d0cc183b828caa96002c265ba1d99c02e8
d4tah0g <k4ff33p0tt@gmail.com>
2019-08-23 20:53:38 +0200

    Change in Generator

--- model.py
+++ model.py
@@ -14,21 +14,21 @@ def generator(input_dim=100,units=1024,activation='relu'):
     # Generator network
     generator = Sequential()
     # FC: 2x2x512
-    generator.add(Dense(2*2*256,input_shape=(input_dim,), kernel_initializer=init))
-    generator.add(Reshape((2, 2, 256)))
+    generator.add(Dense(2*2*1024,input_shape=(input_dim,), kernel_initializer=init))
+    generator.add(Reshape((2, 2, 1024)))
     generator.add(UpSampling2D())
 
     # Conv 2: 8x8x128
-    generator.add(Conv2DTranspose(256, kernel_size=5, strides=2, padding='same'))
+    generator.add(Conv2DTranspose(512, kernel_size=5, strides=2, padding='same'))
     generator.add(BatchNormalization(momentum=0.8))
     generator.add(LeakyReLU(0.2))
 
     # Conv 3: 16x16x64
-    generator.add(Conv2DTranspose(128, kernel_size=5, strides=2, padding='same'))
+    generator.add(Conv2DTranspose(256, kernel_size=5, strides=2, padding='same'))
     generator.add(BatchNormalization(momentum=0.8))
     generator.add(LeakyReLU(0.2))
 
-    generator.add(Conv2DTranspose(64, kernel_size=5, strides=2, padding='same'))
+    generator.add(Conv2DTranspose(128, kernel_size=5, strides=2, padding='same'))
     generator.add(BatchNormalization(momentum=0.8))
     generator.add(LeakyReLU(0.2))
 
@@ -45,21 +45,21 @@ def discriminator(input_shape=(32, 32, 3),nb_filter=64):
 
     # Conv 1: 16x16x64
 
-    discriminator.add(Conv2D(32, input_shape=(64, 64, 3), kernel_size=5, strides=2, padding='same',
+    discriminator.add(Conv2D(64, input_shape=(64, 64, 3), kernel_size=5, strides=2, padding='same',
                             kernel_initializer=init))
     discriminator.add(LeakyReLU(0.2))
 
     # Conv 2:
-    discriminator.add(Conv2D(64, kernel_size=5, strides=2, padding='same'))
-    discriminator.add(BatchNormalization())
-    discriminator.add(LeakyReLU(0.2))
-
-    # Conv 3:
     discriminator.add(Conv2D(128, kernel_size=5, strides=2, padding='same'))
     discriminator.add(BatchNormalization())
     discriminator.add(LeakyReLU(0.2))
 
+    # Conv 3:
     discriminator.add(Conv2D(256, kernel_size=5, strides=2, padding='same'))
+    discriminator.add(BatchNormalization())
+    discriminator.add(LeakyReLU(0.2))
+
+    discriminator.add(Conv2D(512, kernel_size=5, strides=2, padding='same'))
     discriminator.add(BatchNormalization())
     discriminator.add(LeakyReLU(0.2))
 
