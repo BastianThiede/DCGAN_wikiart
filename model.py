fe8b1d6ea1f9e61735d4a9fa9116144661ba671d
d4tah0g <k4ff33p0tt@gmail.com>
2019-08-23 23:24:23 +0200

    Change in Generator

--- model.py
+++ model.py
@@ -20,16 +20,16 @@ def generator(input_dim=100,units=1024,activation='relu'):
 
     # Conv 2: 8x8x128
     generator.add(Conv2DTranspose(512, kernel_size=5, strides=2, padding='same'))
-    generator.add(BatchNormalization())
+    generator.add(BatchNormalization(momentum=0.8))
     generator.add(ReLU())
 
     # Conv 3: 16x16x64
     generator.add(Conv2DTranspose(256, kernel_size=5, strides=2, padding='same'))
-    generator.add(BatchNormalization())
+    generator.add(BatchNormalization(momentum=0.8))
     generator.add(ReLU())
 
     generator.add(Conv2DTranspose(128, kernel_size=5, strides=2, padding='same'))
-    generator.add(BatchNormalization())
+    generator.add(BatchNormalization(momentum=0.8))
     generator.add(ReLU())
 
 
