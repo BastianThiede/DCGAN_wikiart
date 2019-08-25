2b37853ed08465a264d790f531a115314ac2f277
d4tah0g <k4ff33p0tt@gmail.com>
2019-08-25 17:13:25 +0200

    Changes in model

--- model.py
+++ model.py
@@ -50,10 +50,9 @@ def discriminator(input_shape=(32, 32, 3),nb_filter=64):
 
     # Conv 1: 16x16x64
 
-    discriminator.add(Conv2D(32, input_shape=(128, 128, 3), kernel_size=5, strides=2, padding='same',
+    discriminator.add(Conv2D(128, input_shape=(128, 128, 3), kernel_size=5, strides=2, padding='same',
                             kernel_initializer=init))
     discriminator.add(LeakyReLU(0.2))
-    discriminator.add(Dropout(0.25))
 
     discriminator.add(Conv2D(256, kernel_size=5, strides=2, padding='same'))
     discriminator.add(BatchNormalization(momentum=0.8))
