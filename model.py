03f115be77fadd1d483217fde907c7ab2a1f8dfc
d4tah0g <k4ff33p0tt@gmail.com>
2019-08-25 14:41:57 +0200

    Change in generator

--- model.py
+++ model.py
@@ -50,20 +50,20 @@ def discriminator(input_shape=(32, 32, 3),nb_filter=64):
 
     # Conv 1: 16x16x64
 
-    discriminator.add(Conv2D(64, input_shape=(128, 128, 3), kernel_size=5, strides=2, padding='same',
+    discriminator.add(Conv2D(128, input_shape=(128, 128, 3), kernel_size=5, strides=2, padding='same',
                             kernel_initializer=init))
     discriminator.add(LeakyReLU(0.2))
 
-    discriminator.add(Conv2D(128, kernel_size=5, strides=2, padding='same'))
+    discriminator.add(Conv2D(256, kernel_size=5, strides=2, padding='same'))
     discriminator.add(BatchNormalization())
     discriminator.add(LeakyReLU(0.2))
     # Conv 2:
-    discriminator.add(Conv2D(256, kernel_size=5, strides=2, padding='same'))
+    discriminator.add(Conv2D(512, kernel_size=5, strides=2, padding='same'))
     discriminator.add(BatchNormalization())
     discriminator.add(LeakyReLU(0.2))
 
     # Conv 3:
-    discriminator.add(Conv2D(512, kernel_size=5, strides=2, padding='same'))
+    discriminator.add(Conv2D(1024, kernel_size=5, strides=2, padding='same'))
     discriminator.add(BatchNormalization())
     discriminator.add(LeakyReLU(0.2))
 
