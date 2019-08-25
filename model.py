b80953f7c34694730279fdc3f212c5dcbe37d125
d4tah0g <k4ff33p0tt@gmail.com>
2019-08-25 16:34:03 +0200

    Change in generator

--- model.py
+++ model.py
@@ -67,7 +67,7 @@ def discriminator(input_shape=(32, 32, 3),nb_filter=64):
     discriminator.add(BatchNormalization())
     discriminator.add(LeakyReLU(0.2))
 
-    discriminator.add(Conv2D(1536, kernel_size=5, strides=2, padding='same'))
+    discriminator.add(Conv2D(1280, kernel_size=5, strides=2, padding='same'))
     discriminator.add(BatchNormalization())
     discriminator.add(LeakyReLU(0.2))
 
