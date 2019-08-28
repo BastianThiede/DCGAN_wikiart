f59ae0f5c19511a1d2ad44a77eb77a9b491e69b4
Bastian Thiede <bastian_thiede@bonify.de>
2019-08-28 16:41:24 +0200

    Reverted to conv2d transpose

--- model.py
+++ model.py
@@ -12,46 +12,34 @@ def generator(input_dim=100,units=1024,activation='relu'):
     init = RandomNormal(stddev=0.02)
 
     # Generator network
+    momentum_value = 0.8
     generator = Sequential()
     # FC: 2x2x512
-    generator.add(Dense(2*2*1024,input_shape=(input_dim,), kernel_initializer=init))
-    generator.add(Reshape((2, 2, 1024)))
+    generator.add(Dense(2*2*2048,input_shape=(input_dim,), kernel_initializer=init))
+    generator.add(Reshape((2, 2, 2048)))
+    generator.add(UpSampling2D())
 
     # Conv 2: 8x8x128
-    generator.add(Conv2D(512, kernel_size=5, padding='same'))
-    generator.add(BatchNormalization())
+    generator.add(Conv2DTranspose(1024, kernel_size=5, strides=2, padding='same'))
+    generator.add(BatchNormalization(momentum=momentum_value))
     generator.add(ReLU())
-    generator.add(UpSampling2D())
-
 
     # Conv 3: 16x16x64
-    generator.add(Conv2D(512, kernel_size=5, padding='same'))
-    generator.add(BatchNormalization())
+    generator.add(Conv2DTranspose(512, kernel_size=5, strides=2, padding='same'))
+    generator.add(BatchNormalization(momentum=momentum_value))
     generator.add(ReLU())
-    generator.add(UpSampling2D())
 
-    generator.add(Conv2D(256, kernel_size=5, padding='same'))
-    generator.add(BatchNormalization())
+    generator.add(Conv2DTranspose(256, kernel_size=5, strides=2, padding='same'))
+    generator.add(BatchNormalization(momentum=momentum_value))
     generator.add(ReLU())
-    generator.add(UpSampling2D())
 
-    generator.add(Conv2D(128, kernel_size=5, padding='same'))
-    generator.add(BatchNormalization())
+    generator.add(
+        Conv2DTranspose(128, kernel_size=5, strides=2, padding='same'))
+    generator.add(BatchNormalization(momentum=momentum_value))
     generator.add(ReLU())
-    generator.add(UpSampling2D())
 
 
-    generator.add(Conv2D(64, kernel_size=5, padding='same'))
-    generator.add(BatchNormalization())
-    generator.add(ReLU())
-    generator.add(UpSampling2D())
-
-    generator.add(Conv2D(32, kernel_size=5, padding='same'))
-    generator.add(BatchNormalization())
-    generator.add(ReLU())
-    generator.add(UpSampling2D())
-
-    generator.add(Conv2D(3, kernel_size=5, padding='same',
+    generator.add(Conv2DTranspose(3, kernel_size=5, strides=2, padding='same',
                                   activation='tanh'))
     print(generator.summary())
     return generator
@@ -62,31 +50,31 @@ def discriminator(input_shape=(32, 32, 3),nb_filter=64):
     discriminator = Sequential()
 
     # Conv 1: 16x16x64
+    momentum_value = 0.8
 
-    discriminator.add(Conv2D(16, input_shape=(128, 128, 3), kernel_size=5, strides=2, padding='same',
+    discriminator.add(Conv2D(128, input_shape=(128, 128, 3), kernel_size=5, strides=2, padding='same',
                             kernel_initializer=init))
     discriminator.add(LeakyReLU(0.2))
 
-    discriminator.add(Conv2D(32, kernel_size=5, strides=2, padding='same'))
-    discriminator.add(BatchNormalization())
-    discriminator.add(LeakyReLU(0.2))
-    discriminator.add(Dropout(0.25))
-    # Conv 2:
-    discriminator.add(Conv2D(64, kernel_size=5, strides=2, padding='same'))
-    discriminator.add(BatchNormalization())
+    discriminator.add(Conv2D(256, kernel_size=5, strides=2, padding='same'))
+    discriminator.add(BatchNormalization(momentum=momentum_value))
     discriminator.add(LeakyReLU(0.2))
     discriminator.add(Dropout(0.25))
 
+    # Conv 2:
+    discriminator.add(Conv2D(512, kernel_size=5, strides=2, padding='same'))
+    discriminator.add(BatchNormalization(momentum=momentum_value))
+    discriminator.add(LeakyReLU(0.2))
+    discriminator.add(Dropout(0.25))
 
     # Conv 3:
-    discriminator.add(Conv2D(128, kernel_size=5, strides=2, padding='same'))
-    discriminator.add(BatchNormalization())
+    discriminator.add(Conv2D(1024, kernel_size=5, strides=2, padding='same'))
+    discriminator.add(BatchNormalization(momentum=momentum_value))
     discriminator.add(LeakyReLU(0.2))
     discriminator.add(Dropout(0.25))
 
-
-    discriminator.add(Conv2D(256, kernel_size=5, strides=2, padding='same'))
-    discriminator.add(BatchNormalization())
+    discriminator.add(Conv2D(2048, kernel_size=5, strides=2, padding='same'))
+    discriminator.add(BatchNormalization(momentum=momentum_value))
     discriminator.add(LeakyReLU(0.2))
     discriminator.add(Dropout(0.25))
 
