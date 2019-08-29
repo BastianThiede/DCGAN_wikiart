7a3c2b512eab10d9794978813dd11257454b1a16
Bastian Thiede <bastian_thiede@bonify.de>
2019-08-29 09:10:06 +0200

    Input layer change

--- model.py
+++ model.py
@@ -15,8 +15,9 @@ def generator(input_dim=100,units=1024,activation='relu'):
     momentum_value = 0.8
     generator = Sequential()
     # FC: 2x2x512
-    generator.add(Dense(2*2*2048,input_shape=(input_dim,), kernel_initializer=init))
-    generator.add(Reshape((2, 2, 2048)))
+    generator.add(Dense(2* 2* 128,input_shape=(input_dim,), kernel_initializer=init))
+    generator.add(Reshape((2, 2, 128)))
+    generator.add(ReLU())
     generator.add(UpSampling2D())
 
     # Conv 2: 8x8x128
@@ -52,28 +53,28 @@ def discriminator(input_shape=(32, 32, 3),nb_filter=64):
     # Conv 1: 16x16x64
     momentum_value = 0.8
 
-    discriminator.add(Conv2D(128, input_shape=(128, 128, 3), kernel_size=5, strides=2, padding='same',
+    discriminator.add(Conv2D(64, input_shape=(128, 128, 3), kernel_size=5, strides=2, padding='same',
                             kernel_initializer=init))
     discriminator.add(LeakyReLU(0.2))
 
-    discriminator.add(Conv2D(256, kernel_size=5, strides=2, padding='same'))
+    discriminator.add(Conv2D(128, kernel_size=5, strides=2, padding='same'))
     discriminator.add(BatchNormalization(momentum=momentum_value))
     discriminator.add(LeakyReLU(0.2))
     discriminator.add(Dropout(0.25))
 
     # Conv 2:
-    discriminator.add(Conv2D(512, kernel_size=5, strides=2, padding='same'))
+    discriminator.add(Conv2D(256, kernel_size=5, strides=2, padding='same'))
     discriminator.add(BatchNormalization(momentum=momentum_value))
     discriminator.add(LeakyReLU(0.2))
     discriminator.add(Dropout(0.25))
 
     # Conv 3:
-    discriminator.add(Conv2D(1024, kernel_size=5, strides=2, padding='same'))
+    discriminator.add(Conv2D(512, kernel_size=5, strides=2, padding='same'))
     discriminator.add(BatchNormalization(momentum=momentum_value))
     discriminator.add(LeakyReLU(0.2))
     discriminator.add(Dropout(0.25))
 
-    discriminator.add(Conv2D(2048, kernel_size=5, strides=2, padding='same'))
+    discriminator.add(Conv2D(1024, kernel_size=5, strides=2, padding='same'))
     discriminator.add(BatchNormalization(momentum=momentum_value))
     discriminator.add(LeakyReLU(0.2))
     discriminator.add(Dropout(0.25))
