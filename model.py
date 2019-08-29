694d29149282ba63a272e49b92b627df55c54fe7
Bastian Thiede <bastian_thiede@bonify.de>
2019-08-29 09:20:14 +0200

    Input layer change

--- model.py
+++ model.py
@@ -15,8 +15,8 @@ def generator(input_dim=100,units=1024,activation='relu'):
     momentum_value = 0.8
     generator = Sequential()
     # FC: 2x2x512
-    generator.add(Dense(2* 2* 128,input_shape=(input_dim,), kernel_initializer=init))
-    generator.add(Reshape((2, 2, 128)))
+    generator.add(Dense(2* 2* 2048,input_shape=(input_dim,), kernel_initializer=init))
+    generator.add(Reshape((2, 2, 2048)))
     generator.add(ReLU())
     generator.add(UpSampling2D())
 
