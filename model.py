993e438cf0d5d41e0c81edee2f88032e2eafe425
d4tah0g <k4ff33p0tt@gmail.com>
2019-09-01 20:40:54 +0200

    Change in architecture

--- model.py
+++ model.py
@@ -23,11 +23,6 @@ def generator(input_dim=100,units=1024,activation='relu'):
     generator.add(ReLU())
     generator.add(UpSampling2D())
 
-    generator.add(Conv2D(filters=256, kernel_size=4, padding='same'))
-    generator.add(BatchNormalization(momentum=0.6))
-    generator.add(ReLU())
-    generator.add(UpSampling2D())
-
     generator.add(Conv2D(filters=128, kernel_size=4, padding='same'))
     generator.add(BatchNormalization(momentum=0.6))
     generator.add(ReLU())
