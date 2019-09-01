dcb4fe4e4869ab8ad802f50fe20d1c6c913301c6
d4tah0g <k4ff33p0tt@gmail.com>
2019-09-01 23:02:53 +0200

    Change in architecture

--- model.py
+++ model.py
@@ -64,7 +64,6 @@ def discriminator(input_shape=(32, 32, 3),nb_filter=64):
     discriminator.add(Conv2D(8, kernel_size=3, padding='same'))
     discriminator.add(LeakyReLU(0.2))
     discriminator.add(Dropout(0.25))
-    discriminator.add(AveragePooling2D())
 
     discriminator.add(Conv2D(16, kernel_size=3, padding='same'))
     discriminator.add(BatchNormalization(momentum=0.6))
