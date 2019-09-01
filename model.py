10f25c5dc64648137726920de8087e4240369fbe
d4tah0g <k4ff33p0tt@gmail.com>
2019-09-01 22:16:41 +0200

    Change in architecture

--- model.py
+++ model.py
@@ -14,9 +14,9 @@ def generator(input_dim=100,units=1024,activation='relu'):
 
     # Generator network
     generator = Sequential()
-    generator.add(Dense(4*4*512, input_dim=100))
+    generator.add(Dense(4*4*128, input_dim=100))
     generator.add(Activation('tanh'))
-    generator.add(Reshape((4, 4, 512)))
+    generator.add(Reshape((4, 4, 128)))
 
     generator.add(Conv2D(filters=256, kernel_size=4, padding='same'))
     generator.add(BatchNormalization(momentum=0.6))
