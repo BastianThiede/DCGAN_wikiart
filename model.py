3e952fb41d4edce74bfb8c928d5e568a7a1527bb
d4tah0g <k4ff33p0tt@gmail.com>
2019-09-02 23:42:15 +0200

    Added noise layer

--- model.py
+++ model.py
@@ -54,9 +54,9 @@ def discriminator(input_shape=(32, 32, 3),nb_filter=64):
     discriminator = Sequential()
 
     # Conv 1: 16x16x64
-    discriminator.add(GaussianNoise(0.2))
+    discriminator.add(GaussianNoise(0.2, input_shape=(256, 256, 3)))
 
-    discriminator.add(Conv2D(16, input_shape=(256, 256, 3), kernel_size=5, strides=2, padding='same',
+    discriminator.add(Conv2D(16, kernel_size=5, strides=2, padding='same',
                             kernel_initializer=init))
     discriminator.add(LeakyReLU(0.2))
 
