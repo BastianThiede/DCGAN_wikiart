98694cf4e80a66204d6266c5d2215bf79e292f53
d4tah0g <k4ff33p0tt@gmail.com>
2019-09-02 21:09:22 +0200

    Model changes

--- model.py
+++ model.py
@@ -14,41 +14,28 @@ def generator(input_dim=100,units=1024,activation='relu'):
 
     # Generator network
     generator = Sequential()
-    generator.add(Dense(4*4*512, input_dim=100))
-    generator.add(Activation('tanh'))
-    generator.add(Reshape((4, 4, 512)))
-
-    generator.add(Conv2D(filters=256, kernel_size=4, padding='same'))
-    generator.add(BatchNormalization(momentum=0.6))
-    generator.add(ReLU())
+    # FC: 2x2x512
+    generator.add(Dense(2*2*1024,input_shape=(input_dim,), kernel_initializer=init))
+    generator.add(Reshape((2, 2, 1024)))
     generator.add(UpSampling2D())
 
-    generator.add(Conv2D(filters=128, kernel_size=4, padding='same'))
-    generator.add(BatchNormalization(momentum=0.6))
+    # Conv 2: 8x8x128
+    generator.add(Conv2DTranspose(512, kernel_size=5, strides=2, padding='same'))
+    generator.add(BatchNormalization(momentum=0.8))
     generator.add(ReLU())
-    generator.add(UpSampling2D())
 
-    generator.add(Conv2D(filters=64, kernel_size=3, padding='same'))
-    generator.add(BatchNormalization(momentum=0.6))
+    # Conv 3: 16x16x64
+    generator.add(Conv2DTranspose(256, kernel_size=5, strides=2, padding='same'))
+    generator.add(BatchNormalization(momentum=0.8))
     generator.add(ReLU())
-    generator.add(UpSampling2D())
 
-    generator.add(Conv2D(filters=32, kernel_size=3, padding='same'))
-    generator.add(BatchNormalization(momentum=0.6))
+    generator.add(Conv2DTranspose(128, kernel_size=5, strides=2, padding='same'))
+    generator.add(BatchNormalization(momentum=0.8))
     generator.add(ReLU())
-    generator.add(UpSampling2D())
 
-    generator.add(Conv2D(filters=16, kernel_size=3, padding='same'))
-    generator.add(BatchNormalization(momentum=0.6))
-    generator.add(ReLU())
-    generator.add(UpSampling2D())
 
-    generator.add(Conv2D(filters=8, kernel_size=3, padding='same'))
-    generator.add(ReLU())
-    generator.add(UpSampling2D())
-
-    generator.add(Conv2D(filters=3, kernel_size=3, padding='same'))
-    generator.add(Activation('sigmoid'))
+    generator.add(Conv2DTranspose(3, kernel_size=5, strides=2, padding='same',
+                                  activation='tanh'))
     print(generator.summary())
     return generator
 
@@ -59,44 +46,31 @@ def discriminator(input_shape=(32, 32, 3),nb_filter=64):
 
     # Conv 1: 16x16x64
 
-    discriminator.add(GaussianNoise(0.2, input_shape=[256, 256, 3]))
-
-    discriminator.add(Conv2D(4, kernel_size=3, padding='same'))
+    discriminator.add(Conv2D(64, input_shape=(64, 64, 3), kernel_size=5, strides=2, padding='same',
+                            kernel_initializer=init))
     discriminator.add(LeakyReLU(0.2))
-    discriminator.add(Dropout(0.25))
 
-    discriminator.add(Conv2D(8, kernel_size=3, padding='same'))
-    discriminator.add(BatchNormalization(momentum=0.6))
+    # Conv 2:
+    discriminator.add(Conv2D(128, kernel_size=5, strides=2, padding='same'))
+    discriminator.add(BatchNormalization())
     discriminator.add(LeakyReLU(0.2))
-    discriminator.add(Dropout(0.25))
-    discriminator.add(AveragePooling2D())
 
-    discriminator.add(Conv2D(16, kernel_size=3, padding='same'))
-    discriminator.add(BatchNormalization(momentum=0.6))
+    # Conv 3:
+    discriminator.add(Conv2D(256, kernel_size=5, strides=2, padding='same'))
+    discriminator.add(BatchNormalization())
     discriminator.add(LeakyReLU(0.2))
-    discriminator.add(Dropout(0.25))
-    discriminator.add(AveragePooling2D())
 
-    discriminator.add(Conv2D(32, kernel_size=3, padding='same'))
-    discriminator.add(BatchNormalization(momentum=0.6))
+    discriminator.add(Conv2D(512, kernel_size=5, strides=2, padding='same'))
+    discriminator.add(BatchNormalization())
     discriminator.add(LeakyReLU(0.2))
-    discriminator.add(Dropout(0.25))
-    discriminator.add(AveragePooling2D())
-
-    discriminator.add(Conv2D(64, kernel_size=4, padding='same'))
-    discriminator.add(BatchNormalization(momentum=0.6))
-    discriminator.add(LeakyReLU(0.2))
-    discriminator.add(Dropout(0.25))
-    discriminator.add(AveragePooling2D())
 
     # FC
     discriminator.add(Flatten())
 
     # Output
-    discriminator.add(Dense(1,activation='sigmoid'))
+    discriminator.add(Dense(1, activation='sigmoid'))
     print(discriminator.summary())
     return discriminator
-
 def build_gan(config_path):
     config = load_config(config_path)
     g = generator()
--- train.py
+++ train.py
@@ -75,7 +75,7 @@ def main(config_path, save_dir, data_dir):
     print("Total epoch:", config['epochs'], "Number of batches:", num_batches)
     print("-------------------")
 
-    z_pred = np.array([np.random.normal(-1, 1, 100) for _ in range(100)])
+    z_pred = np.array([np.random.normal(0, 0.5, 100) for _ in range(100)])
     y_d_true = [1] * batch_size
     d_loss_fake_data = list()
     d_loss_real_data = list()
@@ -147,7 +147,7 @@ def train_batch(X_train, batch_size, dcgan, discriminator, generator, index,
     X_d_true = X_train[index * batch_size:(index + 1) * batch_size]
 
     #X_d_true = X_d_true.view(dtype=np.float32, type=np.ndarray)
-    X_g = np.array([np.random.normal(-1, 1, 100) for _ in range(batch_size)])
+    X_g = np.array([np.random.normal(0, 0.5, 100) for _ in range(batch_size)])
     X_d_gen = generator.predict(X_g, verbose=0)
     # train discriminator
     d_loss_real = discriminator.train_on_batch(X_d_true, y_d_true)
