9cc9b9f50e2d9ed925f906dbf85a2d87bd41b37d
d4tah0g <k4ff33p0tt@gmail.com>
2019-09-03 00:11:19 +0200

    Added noise layer

--- model.py
+++ model.py
@@ -79,18 +79,6 @@ def discriminator(input_shape=(32, 32, 3),nb_filter=64):
     discriminator.add(LeakyReLU(0.2))
     discriminator.add(AveragePooling2D())
 
-    discriminator.add(
-        Conv2D(256, kernel_size=5, kernel_initializer=init, strides=2,
-               padding='same'))
-    discriminator.add(BatchNormalization())
-    discriminator.add(LeakyReLU(0.2))
-
-    discriminator.add(
-        Conv2D(512, kernel_size=5, kernel_initializer=init, strides=2,
-               padding='same'))
-    discriminator.add(BatchNormalization())
-    discriminator.add(LeakyReLU(0.2))
-
 
     # FC
     discriminator.add(Flatten())
