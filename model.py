aff88768639d227bce9cfc7bc76cadb3f2b7a4e5
Bastian Thiede <bastian_thiede@bonify.de>
2019-09-03 16:20:52 +0200

    Removed Average pooling

--- model.py
+++ model.py
@@ -64,7 +64,6 @@ def discriminator(input_shape=(32, 32, 3),nb_filter=64):
     discriminator.add(Conv2D(32, kernel_size=5, kernel_initializer=init, strides=2, padding='same'))
     discriminator.add(BatchNormalization())
     discriminator.add(LeakyReLU(0.2))
-    discriminator.add(AveragePooling2D())
 
     discriminator.add(Conv2D(64, kernel_size=5, kernel_initializer=init, strides=2, padding='same'))
     discriminator.add(BatchNormalization())
