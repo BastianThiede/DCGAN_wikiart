a95dbdc9a1bc734bde4c9fefcb4a2cf71a96aa93
Bastian Thiede <bastian_thiede@bonify.de>
2019-09-04 13:57:08 +0200

    Added Random initializer

--- model.py
+++ model.py
@@ -55,29 +55,29 @@ def discriminator(input_shape=(32, 32, 3),nb_filter=64):
 
     # Conv 1: 16x16x64
 
-    discriminator.add(Conv2D(16, kernel_size=5, strides=2, padding='same',
+    discriminator.add(Conv2D(16,kernel_initializer=init, kernel_size=5, strides=2, padding='same',
                              input_shape=(256, 256, 3)))
     discriminator.add(LeakyReLU(0.2))
 
 
     # Conv 2:
-    discriminator.add(Conv2D(32, kernel_size=5, strides=2, padding='same'))
+    discriminator.add(Conv2D(32,kernel_initializer=init, kernel_size=5, strides=2, padding='same'))
     discriminator.add(BatchNormalization())
     discriminator.add(LeakyReLU(0.2))
 
-    discriminator.add(Conv2D(64, kernel_size=5, strides=2, padding='same'))
+    discriminator.add(Conv2D(64,kernel_initializer=init, kernel_size=5, strides=2, padding='same'))
     discriminator.add(BatchNormalization())
     discriminator.add(LeakyReLU(0.2))
 
-    discriminator.add(Conv2D(128, kernel_size=5, strides=2, padding='same'))
+    discriminator.add(Conv2D(128,kernel_initializer=init, kernel_size=5, strides=2, padding='same'))
     discriminator.add(BatchNormalization())
     discriminator.add(LeakyReLU(0.2))
 
-    discriminator.add(Conv2D(256, kernel_size=5, strides=2, padding='same'))
+    discriminator.add(Conv2D(256,kernel_initializer=init, kernel_size=5, strides=2, padding='same'))
     discriminator.add(BatchNormalization())
     discriminator.add(LeakyReLU(0.2))
 
-    discriminator.add(Conv2D(512, kernel_size=5, strides=2, padding='same'))
+    discriminator.add(Conv2D(512,kernel_initializer=init, kernel_size=5, strides=2, padding='same'))
     discriminator.add(BatchNormalization())
     discriminator.add(LeakyReLU(0.2))
 
