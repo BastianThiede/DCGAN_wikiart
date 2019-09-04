e59fa91f8a635d6411ff36d89c84e7b1870eeb69
d4tah0g <k4ff33p0tt@gmail.com>
2019-09-04 22:27:13 +0200

    Switched BN momentum

--- model.py
+++ model.py
@@ -62,7 +62,7 @@ def discriminator(input_shape=(32, 32, 3),nb_filter=64):
 
     # Conv 2:
     discriminator.add(Conv2D(32,kernel_initializer=init, kernel_size=5, strides=2, padding='same'))
-    discriminator.add(BatchNormalization())
+    discriminator.add(BatchNormalization(momentum=0.6))
     discriminator.add(LeakyReLU(0.2))
 
     discriminator.add(Conv2D(64,kernel_initializer=init, kernel_size=5, strides=2, padding='same'))
@@ -74,7 +74,7 @@ def discriminator(input_shape=(32, 32, 3),nb_filter=64):
     discriminator.add(LeakyReLU(0.2))
 
     discriminator.add(Conv2D(256,kernel_initializer=init, kernel_size=5, strides=2, padding='same'))
-    discriminator.add(BatchNormalization(momentum=0.6))
+    discriminator.add(BatchNormalization())
     discriminator.add(LeakyReLU(0.2))
 
     discriminator.add(Conv2D(512,kernel_initializer=init, kernel_size=5, strides=2, padding='same'))
