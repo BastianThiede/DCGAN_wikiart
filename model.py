23ee1fe522d09f1e44828d8dfb19917883d5720c
d4tah0g <k4ff33p0tt@gmail.com>
2019-09-09 00:40:42 +0200

    Added average Pooling to discriminator

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 from keras.initializers import RandomNormal
 from keras.layers import (Conv2DTranspose, Conv2D, BatchNormalization, Reshape,
-                          Dense, LeakyReLU, ReLU, GaussianNoise, Dropout,Activation,
+                          Dense, LeakyReLU, ReLU, GaussianNoise, Dropout, Activation,
                           AveragePooling2D, Flatten, UpSampling2D)
 from keras.models import Sequential
 from keras.optimizers import Adam
@@ -9,76 +9,101 @@ from keras.utils import multi_gpu_model
 from utils import load_config
 
 
-def generator(input_dim=100,units=1024,activation='relu'):
+def generator(input_dim=100, units=1024, activation='relu'):
     init = RandomNormal(stddev=0.02)
 
     # Generator network
     generator = Sequential()
     # FC: 2x2x512
-    generator.add(Dense(4*4*1024,input_shape=(input_dim,),
-                        activation='linear',kernel_initializer=init))
+    generator.add(Dense(4 * 4 * 1024, input_shape=(input_dim,),
+                        activation='linear', kernel_initializer=init))
     generator.add(Reshape((4, 4, 1024)))
 
     # Conv 2: 8x8x128
-    generator.add(Conv2DTranspose(512, kernel_size=5,kernel_initializer=init, strides=2, padding='same'))
+    generator.add(
+        Conv2DTranspose(512, kernel_size=5,
+                        kernel_initializer=init, strides=2, padding='same'))
     generator.add(BatchNormalization(momentum=0.7))
     generator.add(ReLU())
 
     # Conv 3: 16x16x64
-    generator.add(Conv2DTranspose(256, kernel_size=5,kernel_initializer=init, strides=2, padding='same'))
-    generator.add(BatchNormalization(momentum=0.7))
-    generator.add(ReLU())
-
-    generator.add(Conv2DTranspose(128, kernel_size=5,kernel_initializer=init, strides=2, padding='same'))
+    generator.add(
+        Conv2DTranspose(256, kernel_size=5,
+                        kernel_initializer=init, strides=2, padding='same')
+    )
     generator.add(BatchNormalization(momentum=0.7))
     generator.add(ReLU())
 
     generator.add(
-        Conv2DTranspose(64, kernel_size=5, strides=2,kernel_initializer=init, padding='same'))
+        Conv2DTranspose(128, kernel_size=5,
+                        kernel_initializer=init, strides=2, padding='same')
+    )
     generator.add(BatchNormalization(momentum=0.7))
     generator.add(ReLU())
 
     generator.add(
-        Conv2DTranspose(32, kernel_size=5, strides=2,kernel_initializer=init, padding='same'))
+        Conv2DTranspose(64, kernel_size=5, strides=2,
+                        kernel_initializer=init, padding='same')
+    )
     generator.add(BatchNormalization(momentum=0.7))
     generator.add(ReLU())
 
-    generator.add(Conv2DTranspose(3, kernel_size=5,kernel_initializer=init, strides=2, padding='same',
-                                  activation='tanh'))
+    generator.add(
+        Conv2DTranspose(32, kernel_size=5, strides=2, kernel_initializer=init,
+                        padding='same')
+    )
+    generator.add(BatchNormalization(momentum=0.7))
+    generator.add(ReLU())
+
+    generator.add(
+        Conv2DTranspose(3, kernel_size=5,
+                        kernel_initializer=init, strides=2, padding='same',
+                        activation='tanh')
+    )
     print(generator.summary())
     return generator
 
-def discriminator(input_shape=(32, 32, 3),nb_filter=64):
+
+def discriminator(input_shape=(32, 32, 3), nb_filter=64):
     init = RandomNormal(stddev=0.02)
 
     discriminator = Sequential()
 
     # Conv 1: 16x16x64
 
-    discriminator.add(Conv2D(16,kernel_initializer=init, kernel_size=5, strides=2, padding='same',
+    discriminator.add(Conv2D(16, kernel_initializer=init, kernel_size=5, strides=2, padding='same',
                              input_shape=(256, 256, 3)))
-
     discriminator.add(LeakyReLU(0.2))
-
+    discriminator.add(AveragePooling2D())
 
     # Conv 2:
-    discriminator.add(Conv2D(32,kernel_initializer=init, kernel_size=5, strides=2, padding='same'))
+    discriminator.add(Conv2D(32, kernel_initializer=init,
+                             kernel_size=5, strides=2, padding='same'))
+    discriminator.add(BatchNormalization())
+    discriminator.add(LeakyReLU(0.2))
+    discriminator.add(AveragePooling2D())
+
+    discriminator.add(Conv2D(64, kernel_initializer=init,
+                             kernel_size=5, strides=2, padding='same'))
+    discriminator.add(BatchNormalization())
+    discriminator.add(LeakyReLU(0.2))
+    discriminator.add(AveragePooling2D())
+
+    discriminator.add(Conv2D(128, kernel_initializer=init,
+                             kernel_size=5, strides=2, padding='same'))
     discriminator.add(BatchNormalization())
     discriminator.add(LeakyReLU(0.2))
 
-    discriminator.add(Conv2D(64,kernel_initializer=init, kernel_size=5, strides=2, padding='same'))
+    discriminator.add(Conv2D(256, kernel_initializer=init,
+                             kernel_size=5, strides=2, padding='same'))
     discriminator.add(BatchNormalization())
     discriminator.add(LeakyReLU(0.2))
 
-    discriminator.add(Conv2D(128,kernel_initializer=init, kernel_size=5, strides=2, padding='same'))
-    discriminator.add(BatchNormalization())
-    discriminator.add(LeakyReLU(0.2))
+    discriminator.add(Conv2D(512, kernel_initializer=init,
+                             kernel_size=5, strides=2, padding='same'))
 
-    discriminator.add(Conv2D(256,kernel_initializer=init, kernel_size=5, strides=2, padding='same'))
     discriminator.add(BatchNormalization())
-    discriminator.add(LeakyReLU(0.2))
 
-    discriminator.add(Conv2D(512,kernel_initializer=init, kernel_size=5, strides=2, padding='same'))
     discriminator.add(LeakyReLU(0.2))
 
     # FC
