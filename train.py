cd6836a3b5a02a378bca221f9a2449c593a70b5d
d4tah0g <k4ff33p0tt@gmail.com>
2019-09-09 10:48:52 +0200

    Added image showing

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
-from utils import get_image_paths, load_data, load_config, save_images, get_gan_paths
+from utils import (get_image_paths, load_data, display_images,
+                   load_config, save_images, get_gan_paths)
 from model import build_gan
 import numpy as np
 from time import time
@@ -9,6 +10,7 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 import pandas as pd
 from collections import Counter
+
 
 def train_batches(config_path, save_dir, data_dir):
     config = load_config(config_path)
@@ -35,7 +37,7 @@ def train_batches(config_path, save_dir, data_dir):
     for epoch in range(epochs):
         start = time()
         for index in tqdm(range(num_batches)):
-            X_train = load_image_batch(paths,batch_size,index)
+            X_train = load_image_batch(paths, batch_size, index)
             d_loss_fake, d_loss_real, g_loss = train_batch(X_train, batch_size,
                                                            dcgan,
                                                            discriminator,
@@ -57,6 +59,7 @@ def train_batches(config_path, save_dir, data_dir):
             dcgan.save(dcgan_path)
             images = generator.predict(z_pred)
             save_images(images, 'dcgan_keras_epoch_{}.png'.format(epoch))
+
 
 def main(config_path, save_dir, data_dir):
     config = load_config(config_path)
@@ -114,8 +117,8 @@ def main(config_path, save_dir, data_dir):
         print('D-loss-real: {}, D-loss-fake: {}, '
               'G-loss: {}, epoch: {}, time: {}\n'
               'D-loss-real-mean: {}, D-loss-fake-mean: {}'.format(
-            d_loss_real, d_loss_fake, g_loss, epoch, end, mean_acc_real,
-            mean_acc_fake))
+                  d_loss_real, d_loss_fake, g_loss, epoch, end, mean_acc_real,
+                  mean_acc_fake))
 
         if epoch % 5 == 0:
             X_d_true = X_train[index * batch_size:(index + 1) * batch_size]
@@ -126,24 +129,19 @@ def main(config_path, save_dir, data_dir):
             disc_preds_true = discriminator.predict(X_d_true)
             disc_preds_fake = discriminator.predict(X_d_gen)
             print()
-            print(Counter(np.round(disc_preds_true[:,0])), 'True_pred_count')
+            print(Counter(np.round(disc_preds_true[:, 0])), 'True_pred_count')
             print(np.mean(disc_preds_true), 'Mean_preds_true')
             print(np.std(disc_preds_true), 'Std_preds_true')
             print(np.mean(y_d_true), 'Mean_preds_true_labels')
-            print(Counter(np.round(y_d_true)),'Counter_true_labels')
+            print(Counter(np.round(y_d_true)), 'Counter_true_labels')
             print('-' * 72)
-            print(Counter(np.round(disc_preds_fake[:,0])), 'Fake_pred_count')
+            print(Counter(np.round(disc_preds_fake[:, 0])), 'Fake_pred_count')
             print(np.mean(disc_preds_fake), 'Mean_preds_fake')
             print(np.std(disc_preds_fake), 'Std_preds_fake')
             print(np.mean(y_d_gen), 'Mean_preds_fake_labels')
-            print(Counter(np.round(y_d_gen)),'Counter_fake_labels')
+            print(Counter(np.round(y_d_gen)), 'Counter_fake_labels')
             print()
-
-
-
-
-
-
+            display_images(X_d_gen)
 
         if epoch % 10 == 0:
             generator.save(generator_path)
